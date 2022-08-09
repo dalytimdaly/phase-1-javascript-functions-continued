7afbe1ba115e1257914dc621d0064e9841f15931
Tim <dalytimdaly@gmail.com>
2022-08-09 09:23:21 -0600

    commit tree 9b19b9271a41bc9a96afa984f27541e5a1ec387a

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+saturdayFun("bathe my dog")
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+mondayWork("work from home")
+
+
+
+function wrapAdjective(symbol = '*') {
+    const innerFunc = function (param1 = "special") {
+        return `You are ${symbol}${param1}${symbol}!`
+    };
+    return innerFunc
+}
+
